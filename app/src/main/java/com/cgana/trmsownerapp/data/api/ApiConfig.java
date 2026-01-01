810b50baf222c2a74a838f04836289382dc9ce86
C-gana <patiencebanda91@gmail.com>
2026-01-01 06:55:17 +0200

    update API base URL for production environment

--- app/src/main/java/com/cgana/trmsownerapp/data/api/ApiConfig.java
+++ app/src/main/java/com/cgana/trmsownerapp/data/api/ApiConfig.java
@@ -2,7 +2,7 @@ package com.cgana.trmsownerapp.data.api;
 
 public class ApiConfig {
     // Change for production
-    public static final String BASE_URL = "http://10.0.2.2:3000/";
+    public static final String BASE_URL = "http://10.81.127.253:3000/";
     public static final long TIMEOUT = 30; // seconds
 }
 
